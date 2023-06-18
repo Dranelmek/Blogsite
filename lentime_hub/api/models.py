af422d3d9ad3cc48962509f7d627ec31585c1a17
Lenard <bebobhd@gmail.com>
2023-06-18 11:23:56 +0100

    getting started on models

--- lentime_hub/api/models.py
+++ lentime_hub/api/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class BlogPost(models.Model):
+    """
+    This model will handel data for
+    the blogs section of the website.
+    """
+
+    heading = models.CharField(max_length=80, default="")
+    body = models.CharField(max_length=500, default="")
+    date = models.DateTimeField(auto_now_add=True)
